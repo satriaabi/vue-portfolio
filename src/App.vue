4d5986245e07980ae5eb7d2d2e257bd93b1c1709
satria.abimanyu <satria.abimanyu@iconpln.co.id>
2024-06-26 11:05:53 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <Navbar />
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>asds</div>
+</template>
+
+<style scoped></style>
